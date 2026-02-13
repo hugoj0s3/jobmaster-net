a62810871b97c383968b587e67c447d1ff88a085
hugoj0s3 <hugo.silva@12twenty.com>
2026-02-13 20:53:32 -0300

    open api generator

--- dashboard/src/openapi-generator.sh
+++ dashboard/src/openapi-generator.sh
@@ -1,4 +1,3 @@
 #Update swagger json everytime to generate new api and models
-rm -rf openapi
-mkdir openapi
-openapi-generator-cli generate -g typescript-axios -i swagger.json --additional-properties=supportsES6=true,withSeparateModelsAndApi=true,modelPackage=models,apiPackage=apis -o ./openapi
+rm -f ./lib/api/schema.d.ts
+npx openapi-typescript swagger.json -o ./lib/api/schema.d.ts
